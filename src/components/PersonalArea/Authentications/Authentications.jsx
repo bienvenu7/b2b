19be1952465a7507ab50f0f178afee45c89726ce
OvvaTech <102521328+OvvaTech@users.noreply.github.com>
2022-07-29 11:22:41 +0300

    paginator for products page

--- src/components/PersonalArea/Authentications/Authentications.jsx
+++ src/components/PersonalArea/Authentications/Authentications.jsx
@@ -1,18 +1,36 @@
 import PersonalAreaLayout from "../PersonalAreaLayout"
 import SvgSelector from "../../../common/icons/SvgSelector"
 import './Authentications.scss'
-import { useEffect, useState } from "react"
+import { useEffect, useMemo, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { useNavigate, useParams } from "react-router-dom"
 import { takeProducts, takeResultStatuses } from "../../../redux/selectors/product-selectors"
 import { addCertificateThunk, getProductsThunk } from "../../../redux/thunks/product-thunk"
 import { setProducts } from "../../../redux/reducers/product-reducer"
-import UploadPhotoModal from "../UploadPhotoModal/UploadPhotoModal"
+import Paginator from "../../Paginator/Paginator"
 
 const Authentications = (props) => {
 
+    const PageSize = 8
 
     const products = useSelector(takeProducts)
+
+    //for pagination
+
+    const [currentPage, setCurrentPage] = useState(1);
+
+    const currentTableData = useMemo(() => {
+        const PageSize = 8
+        const firstPageIndex = (currentPage - 1) * PageSize;
+        const lastPageIndex = firstPageIndex + PageSize;
+        return products && products.slice(firstPageIndex, lastPageIndex);
+    }, [currentPage, products]);
+
+    //
+
+
+
+
     const resultStatuses = useSelector(takeResultStatuses)
 
     const dispatch = useDispatch()
@@ -20,26 +38,28 @@ const Authentications = (props) => {
     const params = useParams()
     const [page, setPage] = useState(null)
 
-    useEffect(()=>{
+    useEffect(() => {
         setPage(params.page === 'completed' ? 'complete' : params.page === 'in-progress' && 'progress')
     })
 
     useEffect(() => {
         products === null && resultStatuses !== null && dispatch(getProductsThunk(
-          {resultStatuses: resultStatuses.filter(el=> params.page === 'completed' ? el.name === 'COMPLETED' : el.name !== 'COMPLETED'),
-          sort: `createdAt:${sortData ? 'DESC' : 'ASC'}`}))
-        
-    },)
+            {
+                resultStatuses: resultStatuses.filter(el => params.page === 'completed' ? el.name === 'COMPLETED' : el.name !== 'COMPLETED'),
+                sort: `createdAt:${sortData ? 'DESC' : 'ASC'}`
+            }))
 
-    useEffect(()=>{
-        return()=>{
+    })
+
+    useEffect(() => {
+        return () => {
             dispatch(setProducts(null))
         }
-    },[])
+    }, [])
 
-    
 
-    
+
+
     const [sortData, setSortData] = useState(true)
     const [searchValue, setSearchValue] = useState()
 
@@ -47,55 +67,64 @@ const Authentications = (props) => {
     function onCompletedClick() {
         setSortData(true)
         navigate('../authentications/completed')
-        const data = { resultStatuses: [
-            resultStatuses.filter(el => el.name === 'COMPLETED')[0]],
-        sort:  'createdAt:DESC'}
+        const data = {
+            resultStatuses: [
+                resultStatuses.filter(el => el.name === 'COMPLETED')[0]],
+            sort: 'createdAt:DESC'
+        }
         dispatch(setProducts(null))
         dispatch(getProductsThunk(data))
     }
 
     function onProgressClick() {
         navigate('../authentications/in-progress')
-        const data = { 
-            sort:  'createdAt:DESC',
-            resultStatuses: resultStatuses.filter(el => el.name !== 'COMPLETED') }
+        const data = {
+            sort: 'createdAt:DESC',
+            resultStatuses: resultStatuses.filter(el => el.name !== 'COMPLETED')
+        }
         dispatch(setProducts(null))
         dispatch(getProductsThunk(data))
     }
 
     function handleSort(sort) {
         setSortData(!sortData)
-        dispatch(getProductsThunk({ 
+        dispatch(getProductsThunk({
             resultStatuses: resultStatuses.filter(el => page === 'progress' ? el.name !== 'COMPLETED' : el.name === 'COMPLETED'),
-            sort: `createdAt:${!sort ? 'DESC' : 'ASC'}` }))
+            sort: `createdAt:${!sort ? 'DESC' : 'ASC'}`
+        }))
     }
 
-    function handleFilter() {
-        const data = {resultStatuses: resultStatuses.filter(el => page === 'progress' ? el.name !== 'COMPLETED' : el.name === 'COMPLETED')
+    function handleSearch() {
+        const data = {
+            resultStatuses: resultStatuses.filter(el => page === 'progress' ? el.name !== 'COMPLETED' : el.name === 'COMPLETED')
         }
-        dispatch(getProductsThunk(searchValue!== '' ? {...data, search: searchValue}: data))
+        dispatch(getProductsThunk(searchValue !== '' ? { ...data, search: searchValue } : data))
     }
 
-    function getPhotoUrl(file){
+    function getPhotoUrl(file) {
         return process.env.NODE_ENV !== 'production' ? '/mockimage.png' : '/assets' + file.path + '/' + file.name
     }
 
-    async function addViewCertificate(el){
+    async function addViewCertificate(el) {
         console.log(el.certificateAvailable)
-        if (el.certificateAvailable !== null){
+        if (el.certificateAvailable !== null) {
             console.log('view')
-         } else{
-             const response = await dispatch(addCertificateThunk(el))
-             !response && navigate('../payment')
-            }
+        } else {
+            const response = await dispatch(addCertificateThunk(el))
+            !response && navigate('../payment')
+        }
     }
 
+    useEffect(() => {
 
-    
+    }, [currentTableData])
+
+
+
 
     return (
         <>
-        
+
             <PersonalAreaLayout>
                 <div className='authent-container'>
                     <div className='authent__buttons-wrapper'>
@@ -105,9 +134,9 @@ const Authentications = (props) => {
                     <div className='authent-wrapper'>
                         <div className='authent__nav-wrapper'>
                             {page === 'progress' ? <div className='authent__nav-label'>In progress authentications</div> : <div className='authent__nav-label'>Completed authentications</div>}
-                            <input className='authent__nav-search' placeholder='Search' onChange={(e) => setSearchValue(e.target.value)} />
+                            <input className='authent__nav-search' placeholder='Search' onChange={(e) => setSearchValue(e.target.value)} onBlur={handleSearch}/>
                             <div className='authent__nav__buttons-wrapper'>
-                                <div className='authent__nav__buttons__elem-wrapper' onClick={handleFilter}><SvgSelector id='filter-icon' />Filter</div>
+                                <div className='authent__nav__buttons__elem-wrapper'><SvgSelector id='filter-icon' />Filter</div>
                                 {page === 'complete' && <div className='authent__nav__buttons__elem-wrapper'><SvgSelector id='download-icon' />Download</div>}
                             </div>
                         </div>
@@ -124,18 +153,18 @@ const Authentications = (props) => {
                                     <div className="authent__table__label__elems-brand">Brand</div>
                                     <div className="authent__table__label__elems-model">Model name</div>
                                     <div className="authent__table__label__elems-outcome">Outcome</div>
-                                    <div className={`authent__table__label__elems-date${!sortData ? ' arrow-up' : ''}`} onClick={()=>handleSort(sortData)}>Submission date <SvgSelector id='down-arrow-icon' /></div>
+                                    <div className={`authent__table__label__elems-date${!sortData ? ' arrow-up' : ''}`} onClick={() => handleSort(sortData)}>Submission date <SvgSelector id='down-arrow-icon' /></div>
                                     <div className="authent__table__label__elems-pdf">Pdf certificate</div>
                                 </div>
                             </div>
-                            {products && products.map((el, index) => <div key={index} className="authent__table__elem">
+                            {currentTableData ? currentTableData.map((el, index) => <div key={index} className="authent__table__elem">
                                 <div className="authent__table__elems-wrapper">
                                     <div className="authent__table__elem-checkbox">
                                         <input key={index} type="checkbox" className='custom-checkbox__table' id={`check-for-elem-${index}`} />
                                         <label htmlFor={`check-for-elem-${index}`} />
                                     </div>
-                                    <div className="authent__table__elem__category" onClick={()=>navigate(`../request/${el.id}`)}>
-                                        <div className="authent__table__elem__category-image" style={{ background: `url(${getPhotoUrl(el)})`}}>
+                                    <div className="authent__table__elem__category" onClick={() => navigate(`../request/${el.id}`)}>
+                                        <div className="authent__table__elem__category-image" style={{ background: `url(${getPhotoUrl(el)})` }}>
                                             {/*<img src={el.image} alt="" />*/}
                                         </div>
                                         <div className="authent__table__elem__category-label">{el.productType.publicName}</div>
@@ -147,9 +176,9 @@ const Authentications = (props) => {
                                     <div className="authent__table__elem-model">{el.modelName}</div>
                                     <div className="authent__table__elem-outcome">{el.checkStatus}</div>
                                     <div className="authent__table__elem-date">{(new Date(el.createdAt)).getDate() + '/' + (Number((new Date(el.createdAt)).getMonth()) + 1) + '/' + (new Date(el.createdAt)).getYear()}</div>
-                                    <div className="authent__table__elem-pdf" onClick={()=>addViewCertificate(el)}>{el.certificateAvailable ? 'View' : 'Add certificate'}</div>
+                                    <div className="authent__table__elem-pdf" onClick={() => addViewCertificate(el)}>{el.certificateAvailable ? 'View' : 'Add certificate'}</div>
                                 </div>
-                            </div>)}
+                            </div>) : 'loader'}
                         </div>}
 
                         {page !== 'complete' && <div className="authent__table">
@@ -162,13 +191,13 @@ const Authentications = (props) => {
                                     <div className="authent__table__label__elems-model">Model name</div>
                                     <div className="authent__table__label__elems-status">Status</div>
                                     <div className="authent__table__label__elems-answer">Answer time</div>
-                                    <div className={`authent__table__label__elems-date${!sortData ? ' arrow-up' : ''}`} onClick={()=>handleSort(sortData)}>Submission date <SvgSelector id='down-arrow-icon' /></div>
+                                    <div className={`authent__table__label__elems-date${!sortData ? ' arrow-up' : ''}`} onClick={() => handleSort(sortData)}>Submission date <SvgSelector id='down-arrow-icon' /></div>
                                 </div>
                             </div>
-                            {products && products.map((el, index) => <div key={index} className="authent__table__elem">
+                            {currentTableData ? currentTableData.map((el, index) => <div key={index} className="authent__table__elem">
                                 <div className="authent__table__elems-wrapper">
-                                    <div className="authent__table__elem__category" onClick={()=>navigate(`../request/${el.id}`)}>
-                                        <div className="authent__table__elem__category-image" style={{ background: `url(${getPhotoUrl(el)})`}}>
+                                    <div className="authent__table__elem__category" onClick={() => navigate(`../request/${el.id}`)}>
+                                        <div className="authent__table__elem__category-image" style={{ background: `url(${getPhotoUrl(el)})` }}>
                                             {/*<img src={el.image} alt="" />*/}
                                         </div>
                                         <div className="authent__table__elem__category-label">{el.productType.publicName}</div>
@@ -182,8 +211,16 @@ const Authentications = (props) => {
                                     <div className="authent__table__elem-answer">{el.answerTime} hours</div>
                                     <div className="authent__table__elem-date">{(new Date(el.createdAt)).getDate() + '/' + (Number((new Date(el.createdAt)).getMonth()) + 1) + '/' + (new Date(el.createdAt)).getYear()}</div>
                                 </div>
-                            </div>)}
+                            </div>) : 'loader'}
+
                         </div>}
+                        {products && currentTableData && <Paginator
+                            className="pagination-bar"
+                            currentPage={currentPage}
+                            totalCount={products.length}
+                            pageSize={PageSize}
+                            onPageChange={page => setCurrentPage(page)}
+                        />}
                     </div>
                 </div>
             </PersonalAreaLayout>
