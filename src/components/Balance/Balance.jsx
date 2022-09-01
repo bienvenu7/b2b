152c24a578d38fc8dc91f60b3c94cfe607f97337
Ovva <pkotofffff@yandex.ru>
2022-09-01 15:28:47 +0300

    форматирование

--- src/components/Balance/Balance.jsx
+++ src/components/Balance/Balance.jsx
@@ -1,32 +1,42 @@
-import { useSelector } from "react-redux"
-import { takeBalance } from "../../redux/selectors/authRequest-selectors"
-import './Balance.scss'
+import { useSelector } from "react-redux";
+import { takeBalance } from "../../redux/selectors/authRequest-selectors";
+import "./Balance.scss";
 
 const Balance = (props) => {
+  const balance = useSelector(takeBalance);
 
-    const balance = useSelector(takeBalance)
+  return (
+    <>
+      <div className="balance-container">
+        <div className="balance-title">
+          Your authentication balance
+          <hr />
+        </div>
+        {balance.length > 0 && (
+          <div className="balance__elems-wrapper">
+            {balance.map(
+              (el, index) =>
+                el.volume > 0 && (
+                  <div key={index} className="balance__elem-wrapper">
+                    <div className="balance__elem-title">
+                      {el.productType.publicName}
+                    </div>
+                    <div className="balance__elem-hours">
+                      {el.answerTime + " hours"}
+                    </div>
+                    <div className="balance__elem-volume">{el.volume}</div>
+                  </div>
+                )
+            )}
+          </div>
+        )}
+        <div className="balance__buttons-wrapper">
+          <button className="balance-button top">Top up now</button>
+          <button className="balance-button">New authentication</button>
+        </div>
+      </div>
+    </>
+  );
+};
 
-    return (
-        <>
-            <div className="balance-container">
-                <div className="balance-title">Your authentication balance
-                    <hr />
-                </div>
-                {balance.length > 0 && <div className="balance__elems-wrapper">
-                    {balance.map((el, index) =>
-                        el.volume > 0 && <div key={index} className="balance__elem-wrapper">
-                            <div className="balance__elem-title">{el.productType.publicName}</div>
-                            <div className="balance__elem-hours">{el.answerTime + ' hours'}</div>
-                            <div className="balance__elem-volume">{el.volume}</div>
-                        </div>)}
-                </div>}
-                <div className="balance__buttons-wrapper">
-                    <button className="balance-button top">Top up now</button>
-                    <button className="balance-button">New authentication</button>
-                </div>
-            </div>
-        </>
-    )
-}
-
-export default Balance
+export default Balance;
--- src/components/Payment/Header/Header.jsx
+++ src/components/Payment/Header/Header.jsx
@@ -1,17 +1,21 @@
-import logo from '../../../common/images/logo-for-mobile.png'
+import logo from "../../../common/images/logo-for-mobile.png";
 
-const Header = (props) =>{
-    return(
-        <>
-        <div className="top_up_bundle__header">
-                        <img src={logo} alt='' className="top_up_bundle__header-logo" />
-                        <div className="top_up_bundle__header__history">
-                            <div className="top_up_bundle__header__history-text bold">Authentication Bundle</div>
-                            <div className="top_up_bundle__header__history-text">&nbsp;{'>'}&nbsp;Payment</div>
-                        </div>
-                    </div>
-        </>
-    )
-}
+const Header = (props) => {
+  return (
+    <>
+      <div className="top_up_bundle__header">
+        <img src={logo} alt="" className="top_up_bundle__header-logo" />
+        <div className="top_up_bundle__header__history">
+          <div className="top_up_bundle__header__history-text bold">
+            Authentication Bundle
+          </div>
+          <div className="top_up_bundle__header__history-text">
+            &nbsp;{">"}&nbsp;Payment
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Header
+export default Header;
