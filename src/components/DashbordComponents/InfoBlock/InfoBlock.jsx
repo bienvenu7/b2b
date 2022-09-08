d396bee792684503c764445fd5c4acb53992023a
Ovva <pkotofffff@yandex.ru>
2022-09-08 14:55:12 +0300

    блок с информацией

--- src/components/DashbordComponents/InfoBlock/InfoBlock.jsx
+++ src/components/DashbordComponents/InfoBlock/InfoBlock.jsx
@@ -1,9 +1,31 @@
 import './InfoBlock.scss'
 import BlockComponentLayout from '../../BlockComponentLayout/BlockComponentLayout'
+import persons from '../../../common/icons/dashboard/persons.png'
+import books from '../../../common/icons/dashboard/books.png'
+import graduationcap from '../../../common/icons/dashboard/graduationcap.png'
 
 const InfoBlock = () => {
   return (
-    <BlockComponentLayout> Info блок </BlockComponentLayout>
+    <BlockComponentLayout>
+      <h2 className='title'>How does the LegitGrails platform work?</h2>
+      <div className='container'>
+        <div className='card'>
+          <img src={persons} alt='#'/>
+          <h3>THE MOST RELIABLE AUTHENTICATION</h3>
+          <p>Your items are reviewed by the most experienced authenticators</p>
+        </div>
+        <div className='card'>
+          <img src={books} alt='#'/>
+          <h3>COHERENT & STREAMLINED ORGANISATION</h3>
+          <p>All authentications are presented in the clearest possible manner</p>
+        </div>
+        <div className='card'>
+          <img src={graduationcap} alt='#'/>
+          <h3>PRACTICAL DATA & ANALYTICS</h3>
+          <p>The data from past authentications can be used to improve your business’ performance</p>
+        </div>
+      </div>
+    </BlockComponentLayout>
   )
 }
 
