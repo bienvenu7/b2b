907a6de45737991c79742dc8c97595217c391dcb
Ovva <pkotofffff@yandex.ru>
2022-09-09 16:01:53 +0300

    условное отображение поля с фильтрацией

--- src/components/DashbordComponents/AuthenticTableBlock/AuthenticTableBlock.jsx
+++ src/components/DashbordComponents/AuthenticTableBlock/AuthenticTableBlock.jsx
@@ -1,29 +1,29 @@
 import "./AuthenticTableBlock.scss";
-import cn from 'classnames'
+import cn from "classnames";
 import { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import SvgSelector from "../../../common/icons/SvgSelector";
 import BlockComponentLayout from "../../BlockComponentLayout/BlockComponentLayout";
 
-
 const AuthenticTableBlock = () => {
-  const {pathname} = useLocation()
-  console.log(pathname)
-  const [searchValue, setSearchValue] = useState()
-  const [titleTable, setTitleTable] = useState("")
+  const { pathname } = useLocation();
+  console.log(pathname);
+  const [searchValue, setSearchValue] = useState();
+  const [titleTable, setTitleTable] = useState("");
+  const [isFilter, setIsFilter] = useState(false);
 
   useEffect(() => {
-    if (pathname === '/dashboard') {
-      setTitleTable("In progress authentications")
+    if (pathname === "/dashboard") {
+      setTitleTable("In progress authentications");
     }
-  }, [])
+  }, []);
 
   const handleSearch = () => {
-    alert("клик по кнопке поиска")
-  }
+    alert("клик по кнопке поиска");
+  };
   const handleFilter = () => {
-    alert("что-то должно произойти при потере фокуса")
-  }
+    setIsFilter(!isFilter);
+  };
 
   return (
     <BlockComponentLayout>
@@ -34,13 +34,63 @@ const AuthenticTableBlock = () => {
             className="authent-table__navigaton__search"
             onChange={(e) => setSearchValue(e.target.value)}
             placeholder="Search"
-            onBlur={handleSearch}
+            // onBlur={handleSearch}
           />
-          <div className="authent-table__navigaton__filter" onClick={handleFilter}>
+          <div
+            className="authent-table__navigaton__filter-btn"
+            onClick={handleFilter}
+          >
             <SvgSelector id="filter-icon" />
           </div>
         </div>
-        
+        <div className={cn("authent-table__filter-wrapper", {
+          visible: isFilter
+        })}>
+          <div className="authent-table__filter-wrapper__elements ">
+            {/* {filterValues &&
+              filterValues.map((el, index) => (
+                <div key={index} className="authent__filter__elem">
+                  <FilterSelect
+                    key={index}
+                    index={index}
+                    mainOptions={mainOptions}
+                    handleChange={handleChange}
+                    length={filterValues.length}
+                  />
+                  {selectedFilter &&
+                  selectedFilter[index] &&
+                  selectedFilter[index].value &&
+                  selectedFilter[index].value === "MODEL" ? (
+                    <input
+                      type="text"
+                      placeholder="model"
+                      onChange={setModelNameValue}
+                      onBlur={(e) => subHandleChange({ value: e.target.value })}
+                    />
+                  ) : (
+                    selectedFilter && (
+                      <Select
+                        key={secondSelectIndex}
+                        onChange={subHandleChange}
+                        classNamePrefix="custom-select__dashboard"
+                        placeholder="Select filter"
+                        options={options && options}
+                      />
+                    )
+                  )}
+                </div>
+              ))} */}
+            <button
+              className="authent-table__filter-wrapper__button"
+              onClick={() => {
+                // setSelectedFilter(null);
+                handleFilter();
+              }}
+            >
+              Clear
+            </button>
+          </div>
+        </div>
       </>
     </BlockComponentLayout>
   );
