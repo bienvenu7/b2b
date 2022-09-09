e294b8d5be7afe2ca84dec85f655c4cb7cb6f0a2
Ovva <pkotofffff@yandex.ru>
2022-09-09 11:02:39 +0300

    переименованны классы в третьем блоке

--- src/components/DashbordComponents/AuthenticBlock/AuthenticBlock.jsx
+++ src/components/DashbordComponents/AuthenticBlock/AuthenticBlock.jsx
@@ -4,24 +4,24 @@ import BlockComponentLayout from '../../BlockComponentLayout/BlockComponentLayou
 const AuthenticBlock = () => {
   return (
     <BlockComponentLayout>
-      <div className='container'>
-        <div className='left-block'>
-          <h3>Authentic score = 75%</h3>
-          <p>Most of your items are authentic yet there is still room for impovement!</p>
-          <button className='button'>Learn how to improve the score</button>
+      <div className='authentic-block__container'>
+        <div className='authentic-block__left-block'>
+          <h3 className='authentic-block__title_big'>Authentic score = 75%</h3>
+          <p className='authentic-block__text_big'>Most of your items are authentic yet there is still room for impovement!</p>
+          <button className='authentic-block__button'>Learn how to improve the score</button>
         </div>
-        <div className='right-block'>
-          <div className='cards'>
-            <div className='cards__item'>
-              <h3 className='cards__item__title'>Authentic items spotted: 75</h3>
-              <p className='cards__item__text'>Your most common authentic items are Gucci bags and Saint Laurent shoes!</p>
+        <div className='authentic-block__right-block'>
+          <div className='authentic-block__cards-block'>
+            <div className='authentic-block__cards-block__item'>
+              <h3 className='authentic-block__title_little'>Authentic items spotted: 75</h3>
+              <p className='authentic-block__text_little'>Your most common authentic items are Gucci bags and Saint Laurent shoes!</p>
             </div>
-            <div className='cards__item'>
-              <h3 className='cards__item__title'>Fake items spotted: 25</h3>
-              <p className='cards__item__text'>Your most common fake items are Prada bags and Dior t-shirts!</p>
+            <div className='authentic-block__cards-block__item'>
+              <h3 className='authentic-block__title_little'>Fake items spotted: 25</h3>
+              <p className='authentic-block__text_little'>Your most common fake items are Prada bags and Dior t-shirts!</p>
             </div>
           </div>
-          <button className='button'>See all verified items</button>
+          <button className='authentic-block__button'>See all verified items</button>
         </div>
       </div>
     </BlockComponentLayout>
