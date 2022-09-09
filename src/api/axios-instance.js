6da7a00a363c103f70b9b48b0dc6600dd8f50a64
Ovva <pkotofffff@yandex.ru>
2022-09-09 16:27:08 +0300

    новый URL в instance axios

--- src/api/axios-instance.js
+++ src/api/axios-instance.js
@@ -2,5 +2,5 @@ import * as axios from "axios";
 import Cookies from "js-cookie";
 
 export const instance = axios.create({
-  baseURL: " http://3.8.69.175:90/",
+  baseURL: "http://3.8.69.175:90",
 });
