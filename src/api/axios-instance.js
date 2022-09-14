340bca7560452e3c530ef2a99685328b274ff22b
Ovva <pkotofffff@yandex.ru>
2022-09-14 09:03:01 +0300

    поменян адрес на тестовый

--- src/api/axios-instance.js
+++ src/api/axios-instance.js
@@ -3,7 +3,7 @@ import Cookies from "js-cookie";
 
 export const instance = axios.create({
 
-  baseURL: "http://3.8.69.175:90"//"http://3.8.69.175:90",
+  baseURL: "https://b2b-portal-dev.herokuapp.com/"//"http://3.8.69.175:90",
 })
 
 // https://b2b-portal-dev.herokuapp.com/ api для тестовых данных
