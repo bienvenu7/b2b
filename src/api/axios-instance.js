b3cb8d76783f07115d9f8bcef7294889b4487940
bienvenu7 <ngadzieb@gmail.com>
2022-09-15 11:41:55 +0300

    mergin dev

--- src/api/axios-instance.js
+++ src/api/axios-instance.js
@@ -6,5 +6,7 @@ export const instance = axios.create({
   baseURL: "https://b2b-portal-dev.herokuapp.com/"//"http://3.8.69.175:90",
 })
 
+
+// https://business.legitgrails.com/api - ИСПОЛЬЗОВАТЬ ЭТО!!!!!
 // https://b2b-portal-dev.herokuapp.com/ api для тестовых данных
 // http://3.8.69.175:90 настоящий бэкенд
