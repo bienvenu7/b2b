f7160d2dd0fb90fea7ce2bbea02017beb624ab1b
OvvaTech <it@ovva.ru>
2022-10-05 15:26:41 +0300

    fix btn notif

--- src/components/notifications/CardNotif.jsx
+++ src/components/notifications/CardNotif.jsx
@@ -3,38 +3,18 @@ import { useState } from "react";
 import { getProduct } from "../../api/product/product-api";
 
 import "./Notif.scss";
+import { useNavigate } from "react-router-dom";
+
 
 const CardNotif = ({ item, product }) => {
-
-  const [prod, setProd] = useState(null)
-
-
-
-
-  useEffect(() => {
-    getOneProduct()
-  }, [item])
-  
-  const getOneProduct = async () => {
-    try {
-      const response = await getProduct(item.productId)
-      setProd(response.data)
-    } catch (error) {
-      
-    }
-  }
-
-  // const newArray = product.filter((p) => p.id === item.productId)
-
-  // console.log(newArray)
-  // console.log(prod)
+  const navigate = useNavigate();
+  const prod = item.product
 
   return (
     <div className="card-content">
-      {/* {console.log(item)} */}
       <div className="card-title">{item.title}</div>
       <div className="card-text">{item.text}</div>
-      {prod?.reasons !== null & prod?.systemStatus === 'AWAIT_PHOTOS' ? <button className="card-btn">Add more photos</button> : ''}
+      {prod?.reasons !== null & prod?.systemStatus === 'AWAIT_PHOTOS' ? <button onClick={() => navigate('/request/'+prod.id)} className="card-btn">Add more photos</button> : ''}
     </div>
   );
 };
